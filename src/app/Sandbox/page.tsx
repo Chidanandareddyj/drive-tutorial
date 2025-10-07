51e703b83dae2bc95e9bab03e62708897ebc8a08
Jonnala Chidananda Reddy <jlcreddy17@gmail.com>
2025-10-07 22:08:03 +0530

    DB schema updates

---
+++ src/app/Sandbox/page.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { db } from '~/server/db'
+import { mockFiles,mockFolders } from '~/lib/mock-data'
+import { files, folders } from '~/server/db/schema'
+import { mock } from 'node:test'
+
+
+const page = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+      <h1>Sandbox Page</h1>
+      <form action={async()=>{
+        "use server"
+        // const users = await db.user.findMany()
+        const folderinsert=await db.insert(folders).values(
+
+            mockFolders.map((folder,index)=>({
+                id: index+1,
+                name: folder.name,
+                parent:index || 0
+            }))
+        )
+        console.log("folders", folderinsert)
+        const fileinsert=await db.insert(files).values(
+
+            mockFiles.map((file,index)=>({
+                id: index+1,
+                name: file.name,
+                size: file.size,
+                url: file.url,
+                parent:index || 0,
+                folder:index || 0
+            }))
+        )
+
+        console.log("files", fileinsert)
+      }}>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default page
